2b3c3cbeb502ec5c09c201a7359bff188d6032c5
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-19 13:12:54 +0530

    control statements

---
+++ dxc/src/controlstatements/IfExample.java
@@ -0,0 +1,11 @@
+package controlstatements;
+
+public class IfExample {
+	public static void main(String[] args) {
+		int age=50;
+		if(age>20) {
+			System.out.println("Age is greater than 70");
+		}
+	}
+
+}
