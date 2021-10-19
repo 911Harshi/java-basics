4ae2c8576c69d1c3b019fef86999ad9e2f50e574
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-19 15:00:14 +0530

    Control Statements

---
+++ dxc/src/controlstatements/ForExample.java
@@ -0,0 +1,13 @@
+package controlstatements;
+
+public class ForExample {
+	public static void main(String[] args) {  
+		// TODO Auto-generated method stub  
+		int sum = 0;  
+		for(int i = 1; i<=10; i++) {  
+		sum = sum + i;  
+		}  
+		System.out.println("The sum of first 10 natural numbers is " + sum);  
+
+}
+}
