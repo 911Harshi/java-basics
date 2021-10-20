11e455e0e1c3a02db996732898c62da9f3c20299
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-20 10:22:02 +0530

    Methods in java

---
+++ dxc/src/basics/Student.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Student {
+	String name;
+	int age;
+	static String COLLEGE_NAME = "NIT";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
