97dd5b880df4ebcb67193a5ce2bbe10c7dfc512f
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-27 12:38:05 +0530

    Collections

---
+++ dxc/src/collections/TreeSet3.java
@@ -0,0 +1,15 @@
+package collections;
+import java.util.*;  
+
+public class TreeSet3 {
+	public static void main(String args[]){  
+		 TreeSet<Integer> set=new TreeSet<Integer>();  
+		         set.add(24);  
+		         set.add(66);  
+		         set.add(12);  
+		         set.add(15);  
+		         System.out.println("Highest Value: "+set.pollLast());  
+		         System.out.println("Lowest Value:  "+set.pollFirst());  
+		 }  
+
+}
