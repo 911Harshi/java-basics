795d6f0830dc63dcf2513d83bf6777ec2f2274b8
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-27 15:05:59 +0530

    Setup and tear down

--- dxc/src/abstraction/BmwTest.java
+++ dxc/src/abstraction/BmwTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
 public class BmwTest extends TestCase {
 	@Test
 	public void testAdd() {
+		System.out.println("Iam in test add method");
 		int expected = 30;
 		int actual = BmwFlyingCar.add(10, 20);
 		assertEquals(expected, actual);
@@ -13,11 +14,27 @@ public class BmwTest extends TestCase {
 
 	@Test
 	public void testElgibility() {
+		System.out.println("Iam in test eligi method");
 		boolean expected = false;
 		boolean actual = BmwFlyingCar.isEligibletoVote(18);
 
 		//assertFalse(actual);
 		assertTrue(actual);
 	}
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		System.out.println("Iam  setting up the resources");
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+		System.out.println("Iam  Releasing up the resources");
+	}
+	
 
 }
