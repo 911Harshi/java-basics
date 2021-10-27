3552203f4aeb9ae630f74414af46c3801c5514d8
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-27 14:33:07 +0530

    Unit Test

--- dxc/src/abstraction/BmwTest.java
+++ dxc/src/abstraction/BmwTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestCase;
 public class BmwTest extends TestCase {
 	@Test
 	public void testAdd() {
-		int expected = 40;
+		int expected = 30;
 		int actual = BmwFlyingCar.add(10, 20);
 		assertEquals(expected, actual);
 	}
