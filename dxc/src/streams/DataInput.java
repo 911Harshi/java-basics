bbc80e82dce05bdfba4481349bb67e851b848df9
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-28 15:37:36 +0530

    Dataoutputstream

---
+++ dxc/src/streams/DataInput.java
@@ -0,0 +1,17 @@
+package streams;
+import java.io.*;   
+
+public class DataInput {
+	 public static void main(String[] args) throws IOException {  
+		    InputStream input = new FileInputStream("D:\\testout.txt");  
+		    DataInputStream inst = new DataInputStream(input);  
+		    int count = input.available();  
+		    byte[] ary = new byte[count];  
+		    inst.read(ary);  
+		    for (byte bt : ary) {  
+		      char k = (char) bt;  
+		      System.out.print(k+"-");  
+		    }  
+		  }  
+
+}
