b0069a840007aed0b3419c5d2f0b6dabf9f3b9a3
Harshitha <Harshitha@LAPTOP-DSJJ3QIJ>
2021-10-28 15:29:39 +0530

    Byte array input and output stream

---
+++ dxc/src/streams/ByteArrayOutput.java
@@ -0,0 +1,21 @@
+package streams;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
+public class ByteArrayOutput {
+	public static void main(String args[])throws Exception{    
+	      FileOutputStream fout1=new FileOutputStream("D:\\f1.txt");    
+	      FileOutputStream fout2=new FileOutputStream("D:\\f2.txt");    
+	        
+	      ByteArrayOutputStream bout=new ByteArrayOutputStream();    
+	      bout.write(85);    
+	      bout.writeTo(fout1);    
+	      bout.writeTo(fout2);    
+	        
+	      bout.flush();    
+	      bout.close();//has no effect    
+	      System.out.println("Success...");    
+	     }    
+
+}
